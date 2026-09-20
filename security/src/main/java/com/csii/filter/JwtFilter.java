b271379528c4d111f1063739800701b6af347889
JK20252651 <461762432@qq.com>
2026-09-20 14:21:23 +0800

    security add jwt

--- security/src/main/java/com/csii/filter/JwtFilter.java
+++ security/src/main/java/com/csii/filter/JwtFilter.java
@@ -20,7 +20,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 
 @Slf4j
